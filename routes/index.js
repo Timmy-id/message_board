1e0443114af2b6f5188874577c2f7294375cbfa6
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2020-12-17 22:45:56 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {title: 'Add a new message', })
+})
+
+router.post('/new', function(req, res, next) {
+  messages.push({ text: req.body.author, user: req.body.message, added: new Date() });
+  res.redirect('/');
+})
+
+module.exports = router;
